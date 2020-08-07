f8f3637302a8096d716d6913d66d75804a3592e9
Zainab Omar <zainab.alshaikhli01@gmail.com>
2020-08-07 00:56:05 +0000

    Done.

---
+++ lib/concerns/memorable.rb
@@ -0,0 +1,14 @@
+module Memorable
+    def reset_all
+      all.clear
+    end
+    
+    def count
+      all.count
+    end
+  module InstanceMethods
+    def initialize
+      self.class.all << self
+    end
+  end
+end
